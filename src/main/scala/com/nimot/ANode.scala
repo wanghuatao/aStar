5daf9371207689f12a06b9a60f814675e22dfb13
Johannes Tomin <johannes.tomin@web.de>
2013-10-15 19:22:18 +0200

    Update ANode.scala

--- src/main/scala/com/nimot/ANode.scala
+++ src/main/scala/com/nimot/ANode.scala
@@ -2,25 +2,39 @@ package com.nimot
 
 
 /**
- * Created by IntelliJ IDEA.
- * User: jtomin
- * Date: 31.10.11
- * Time: 19:30
- * To change this template use File | Settings | File Templates.
+ * This class is representing a single node of underlying
+ * graph, characterised by:
+ * 
+ *  position (x,y)
+ *  any data
  */
 
 class ANode[A](val x: Int, val y: Int, data: A) {
 
-  var f: Float = _
-
+ /*
+ * Cost of the  past path, which is the known distance 
+ * from the starting node to the current node
+ */
   var g: Float = _
 
+ /*
+ *  Cost of the future path, which is a heuristic
+ *  estimate of the distance from this node to the goal
+ */
   var h: Float = _
 
+  /*
+  * Cost of current node is a sum of  g and h, which is being used 
+  * to determine the order in which the search visits nodes in the tree
+  */
+  var f: Float = _
+  
+  
   var row: Int = _
 
   var col: Int = _
 
+  // Privious node in the alreday determined path
   var parentNode:Option[ANode[A]] = None
 
   var traversable: Boolean = true
